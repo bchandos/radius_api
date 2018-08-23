ecb1ab8ff3a82f0c7a6e64c02b5f9ca979c0c86f
bchandos <bill.chandos@gmail.com>
2018-08-23 14:36:37 -0700

    update project URL to github page

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A Radius CRM web services wrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/bchandos/radius_api",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3.6",
